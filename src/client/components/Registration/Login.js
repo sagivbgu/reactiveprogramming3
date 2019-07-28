c6fd37c4250c76c36c9bdd3caba5d3c02d1ba061
S <sagivbgu>
2019-07-28 21:28:04 +0300

    Minor change

--- src/client/components/Registration/Login.js
+++ src/client/components/Registration/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link, withRouter} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {connect} from 'react-redux';
 import './login.scss';
 import actions from './actions';
