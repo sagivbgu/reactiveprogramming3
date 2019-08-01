a6d92d26321297170daecfa005785c85100bebd5
S <sagivbgu>
2019-08-01 10:56:44 +0300

    Update user profile - Error disappears on success

--- src/client/components/Profile/reducer.js
+++ src/client/components/Profile/reducer.js
@@ -6,7 +6,7 @@ const ProfileReducer = (state = initialState.profile, action) => {
     switch (action.type) {
         case ProfileActionsConstants.FETCH_PROFILE_SUCCESS:
         case ProfileActionsConstants.UPDATE_PROFILE_SUCCESS:
-            return state.mergeDeep(action.payload);
+            return state.mergeDeep(action.payload).set('error','');
         case ProfileActionsConstants.FETCH_PROFILE_FAILURE:
         case ProfileActionsConstants.UPDATE_PROFILE_FAILURE:
             return state.set('error', action.payload);
