e97aecea9345e1001978676651a01dd47b1544ba
sagivbgu <sagivsc@post.bgu.ac.il>
2019-08-01 18:36:17 +0300

    Minor change

--- src/server/server.js
+++ src/server/server.js
@@ -1,45 +1,42 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const { resolve } = require('path');
+const {resolve} = require('path');
 const fs = require('fs');
 const path = require('path');
 
 
-
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/ReactiveProgramming3',
-  port: 8000
+    mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/ReactiveProgramming3',
+    port: 8000
 };
 
 //setup database
 mongoose.Promise = global.Promise;
 // MongoDB Connection
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect(config.mongoURL, { useNewUrlParser: true }, (error) => {
-    if (error) {
-      console.error('Please make sure Mongodb is installed and running!');
-      throw error;
-    }else console.log('connected to database!');
-  });
+    mongoose.connect(config.mongoURL, {useNewUrlParser: true}, (error) => {
+        if (error) {
+            console.error('Please make sure Mongodb is installed and running!');
+            throw error;
+        } else console.log('connected to database!');
+    });
 }
 mongoose.set('useFindAndModify', false);
-
 
 
 const app = express();
 
 //body parser for json. must be done before API routes
 app.use(bodyParser.json({limit: '50mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended:true})); //handle body requests
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true})); //handle body requests
 console.log(__dirname);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
 // Add backend api routes
 fs.readdirSync(__dirname + '/api').forEach((file) => {
-  require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
+    require(`./api/${file.substr(0, file.indexOf('.'))}`)(app);
 });
 
 
